a786c058e9904d406b41fb102450561e6570349a
DEBRIS APRON <debrisapron@gmail.com>
2015-05-16 14:38:06 -0700

    Add bob

---
+++ app/models/bob.rb
@@ -0,0 +1,19 @@
+module Bob
+
+  RULES = {
+    [/^[^a-z]*$/, /[A-Z]+/] => 'Woah, chill out!',
+    [/\?$/]                 => 'Sure.',
+    [/^\s*$/]               => 'Fine. Be that way!'
+  }
+
+  def self.respond_to(msg)
+    matching_rules = RULES.select do |patts, resp|
+      patts.all? { |patt| msg =~ patt }
+    end
+
+    # Silly dance to avoid if, unless and (just in case) iif
+    matching_responses = (matching_rules.any? && matching_rules.values) || ['Whatever.']
+    matching_responses.join(' ')
+  end
+
+end
---
+++ spec/models/bob_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+RSpec.describe Bob do
+
+  it "responds to a question with 'Sure.'" do
+    resp = Bob.respond_to('Will it blend?')
+    expect(resp).to eq('Sure.')
+
+    # I think '?' counts as a question in the context of internet chat
+    resp = Bob.respond_to('?') 
+    expect(resp).to eq('Sure.')
+  end
+
+  it "responds to all-caps messages with 'Woah, chill out!'" do
+    resp = Bob.respond_to('WILL IT BLEND')
+    expect(resp).to eq('Woah, chill out!')
+    resp = Bob.respond_to('WILL IT F1#%2@G BLEND')
+    expect(resp).to eq('Woah, chill out!')
+  end
+
+  it "responds to all-caps questions with 'Whoah, chill out! Sure.'" do
+    resp = Bob.respond_to('WILL IT BLEND?')
+    expect(resp).to eq('Woah, chill out! Sure.')
+    resp = Bob.respond_to('WILL IT F1#%2@G BLEND?')
+    expect(resp).to eq('Woah, chill out! Sure.')
+  end
+
+  it "responds to empty or all-whitespace messages with 'Fine. Be that way!'" do
+    resp = Bob.respond_to('')
+    expect(resp).to eq('Fine. Be that way!')
+    resp = Bob.respond_to('   ')
+    expect(resp).to eq('Fine. Be that way!')
+    resp = Bob.respond_to("  \n ")
+    expect(resp).to eq('Fine. Be that way!')
+  end
+
+  it "responds to any other messages with 'Whatever.'" do
+    resp = Bob.respond_to('Will it blend')
+    expect(resp).to eq('Whatever.')
+    resp = Bob.respond_to('*%*&^%$$$£& )(&*^')
+    expect(resp).to eq('Whatever.')
+    resp = Bob.respond_to('1')
+    expect(resp).to eq('Whatever.')
+  end
+
+end
