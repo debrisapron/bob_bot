ccae36636abf43badbbba180abf688754b44a1fc
DEBRIS APRON <debrisapron@gmail.com>
2015-05-18 16:03:10 -0700

    Add cookie authentication of message posts

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,14 +2,15 @@ class MessagesController < ApplicationController
 
   # POST /api/messages
   def create
-    Message.create!(allowed_params)
+    current_user = User.find_by(token: cookies['user_token'])
+    Message.create!(allowed_params.merge(user_id: current_user.id))
     head :ok
   end
 
   private
 
   def allowed_params
-    @allowed_params ||= params.require(:message).permit(:user_id, :text)
+    @allowed_params ||= params.require(:message).permit(:text)
   end
 
 end
--- spec/requests/messages_request_spec.rb
+++ spec/requests/messages_request_spec.rb
@@ -5,7 +5,8 @@ describe "Messages API" do
   context "POST" do
     it "creates a new message and returns OK" do
       user = User.create!
-      post '/api/messages', { message: { user_id: user.id, text: 'Foobar' } }
+      cookies['user_token'] = user.token
+      post '/api/messages', { message: { text: 'Foobar' } }
       expect(response).to be_success
       expect(Message.find_by(user: user, text: 'Foobar')).to_not be_nil
     end
