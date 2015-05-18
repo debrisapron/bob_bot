5ffb241ba5f112b276c578e6ad5321af004168de
DEBRIS APRON <debrisapron@gmail.com>
2015-05-18 14:03:28 -0700

    Add failing JSON test

--- spec/requests/api/users_request_spec.rb
+++ spec/requests/api/users_request_spec.rb
@@ -6,6 +6,9 @@ describe "Users API" do
     it "creates a new user and returns them as JSON" do
       post '/api/users'
       expect(response).to be_success
+      data = JSON.parse(response.body)
+      expect(data.id).to exist
+      expect(data.name).to equal("User#{ data.id }")
     end
   end
 
