32dac4c3bfe3e73a54cae81f6278d527edb07148
DEBRIS APRON <debrisapron@gmail.com>
2015-05-19 19:14:45 -0700

    Refactor notification, move into Message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,6 +5,20 @@ class Message < ActiveRecord::Base
   validates :user, presence: true
   validates :text, presence: true
 
+  after_create :notify_subscribers
+
   scope :since, ->(t) { where('created_at > ?', t).order(:created_at) }
+
+  private
+
+  # REFACTOR putting this here for simplicity, however this is _not_
+  # a business domain concern, it is infrastructure, and should be
+  # moved to an observer or similar
+  def notify_subscribers
+    # WORKAROUND Not sure why I have to do this dance for a simple publish,
+    # but the faye-rails docs are really unhelpful and I'm tired
+    client = Faye::Client.new('http://localhost:3000/faye')
+    client.publish('/public_message', {})
+  end
 
 end
